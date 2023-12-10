703eadec738fb83258df45dfebe57c8ccbc5943f
Joseph <yosefcassell@gmail.com>
2023-12-09 21:02:26 -0800

    spots

--- backend/db/migrations/20231129004243-create-spot.js
+++ backend/db/migrations/20231129004243-create-spot.js
@@ -1,5 +1,8 @@
 'use strict';
 let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -70,6 +73,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    return queryInterface.dropTable(options);
   }
 };
